7cba261aa29268623c57e953fe279d91792a5e4c
dolphinchemia <dolphinchemia@gmail.com>
2018-01-26 17:31:47 +0530

    initial commit

---
+++ Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace mDolphin
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
